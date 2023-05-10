79d48b3947bb108f50fa1334ddc7f099b1813800
Anastasiya McElnikova <sav20043+McElnikova@yandex.ru>
2023-05-10 21:55:20 +0300

    fix: Домашнее задание готово

--- assets/js/task3.js
+++ assets/js/task3.js
@@ -1,54 +1,70 @@
+const startYear=1582;//Год введения Григорианского календаря
+const finishYear=500000;//Ограничение сверху
+
 checkLeap.onclick=()=>{
-    let leap=false;
-    let nYear=year.value;
-    checkYearCorrect(nYear);
-  }
-  
-  year.onkeyup=function(){
-    /*Почему когда нажимаю на input точку приходит value=""??, хотя в input эта точка стоит.
-    
-    */
-    let inputData=this.value;
-    inputData=inputData.replace(/[^0-9]/g, '');
-    this.value=inputData;
-    setMessage(inputData);
-    
-    return true;
-  }
-  
-  
-  function checkYearCorrect(year)
-  //Проверка года на корректность
+  if (checkYearCorrect(year.value))
   {
-    const startYear=1582;
-    const finishYear=5000;
-    if(!year){
-      setMessage("");
-      return false;
+    if(isLeap(year.value)){
+      setMessage('Год является високосным');
     }
-    else if(year<startYear)
-    {
-      setMessage("В данном году Григорианский стиль не применялся");
-      return false;
+    else{
+      setMessage('Год не високосный');
     }
-    else if(year>finishYear)
-    {
-      setMessage("Накопленная ошибка в датах не позволяет применять Григорианский стиль");
-      return false;
-    }  
-    return true;
   }
   
-  
-  function setMessage(message)
-  //Функция отображения сообщений
+}
+
+year.onkeydown=function(e){
+
+  if(e.keyCode==13)  checkLeap.onclick(e);
+
+  if(e.keyCode!=8 && e.keyCode!=0 && !((e.keyCode>=0x30 && e.keyCode<=0x39) || (e.keyCode>=0x60 && e.keyCode<=0x69))){
+			e.preventDefault();
+		}
+}
+
+year.oninput=function (e){
+  if(e.data==".")e.preventDefault();
+  this.value=this.value.replace(/[^0-9]/g, '');
+}
+
+
+function checkYearCorrect(year)
+//Проверка года на корректность
+{
+  if(!year){
+    setMessage("");
+    return false;
+  }
+  else if(year<startYear)
   {
-    if (!message.length){
-      result1.style="";
-    }
-    else
-    {
-      result1.style="background-color: var(--bg-mess-color)";
-    }
-    result1.innerHTML=message;
+    setMessage("В данном году Григорианский стиль не применялся");
+    return false;
   }
+  else if(year>finishYear)
+  {
+    setMessage("Накопленная ошибка в датах не позволяет применять Григорианский стиль");
+    return false;
+  }  
+  return true;
+}
+
+
+function setMessage(message)
+//Функция отображения сообщений
+{
+  if (!message.length){
+    result1.style="";
+  }
+  else
+  {
+    result1.style="background-color: var(--bg-mess-color)";
+  }
+  result1.innerHTML=message;
+}
+
+function isLeap(nYear)
+//Проверка на високосный год
+{
+  return !(nYear%400) || (nYear%100 && !(nYear%4));
+}
